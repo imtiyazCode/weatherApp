ec7f9fe79c9b0278dd684212774ca6bd20e7adc9
Imtiyaz <76092300+IntiTalent@users.noreply.github.com>
2023-01-27 14:49:52 +0530

    final wrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState } from 'react';
 import { fetchWeather } from './api';
+import WeatherCard from './Components/WeatherCard';
 
 function App() {
   const [city, setCity] = useState('');
@@ -23,7 +24,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1 style={{ textAlign: 'center' }}>Weather App</h1>
+      <h1 className='app_heading'>Weather App</h1>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
@@ -37,35 +38,7 @@ function App() {
       {error ? (
         <p className='error'>{error}</p>
       ) : (
-        <div className="weather-card">
-          <div className="card-header">
-            <div>
-              <p className="weather-city">{weather ? (weather.name + " " + weather.sys.country) : "-"}</p>
-              <p className="weather-description">{weather ? (weather.weather[0].description) : "-"}</p>
-            </div>
-            <img alt={'weather'} className={'weather-icon'} src={`icons/${weather ? (weather.weather[0].icon) : "unknown"}.png`} />
-          </div>
-          <div className="card-body">
-            <h2 className="weather-temp">{weather ? (weather.main?.temp - 273.15).toFixed(0) : ''}°C</h2>
-            <div className="weather-details">
-              <div className="parameters">
-                <span className="param-label">Details</span>
-              </div>
-              <div className="parameters">
-                <span className='param-label'>Feels Like:</span>
-                <span className='param-val'> {weather ? (weather.main?.feels_like - 273.15).toFixed(2) : '-'} °C</span>
-              </div>
-              <div className="parameters">
-                <span className='param-label'>Humidity: </span>
-                <span className='param-val'> {weather ? (weather.main?.humidity) : '-'} %</span>
-              </div>
-              <div className="parameters">
-                <span className='param-label'>Pressure: </span>
-                <span className='param-val'> {weather ? (weather.main?.pressure) : '-'} hPs</span>
-              </div>
-            </div>
-          </div>
-        </div>
+        <WeatherCard weather={weather} />
       )}
     </div>
   );
