4f66189f6056d1491145ea01a0aca2d23eea4f44
Surendra Goswami <surendrag879@gmail.com>
2023-01-30 14:34:39 +0530

    first commit

--- src/api.js
+++ src/api.js
@@ -1,12 +1,11 @@
 import axios from "axios";
-
 export async function fetchWeather(city, setError) {
-  console.log(process.env.REACT_APP_API_KEY);
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=77a97069fcd0d900a1d2c5abc27f4d38`;
 
   try {
     const response = await axios.get(url);
     setError("");
+    console.log(response.data)
     return response.data;
   } catch (error) {
     setError("City Not Found!");
